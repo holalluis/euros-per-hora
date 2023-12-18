874069ecd5cad020d92cc93f7a99a50a65c90ca5
Lluís Bosch <holalluis@gmail.com>
2023-12-18 17:21:37 +0100

    nou commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # euros-per-hora
+
+calcula ràpidament quants euros per hora et gastes en tonteries cada mes
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#git
+git add *
+git commit -am 'nou commit'
+git push
+
+#desplega la web al servidor
+# -h  human readable format
+# -P  mostra progrÃ©s
+# -vv incrementa verbositat
+# -r  actua recursivament
+rsync \
+  -hPvr \
+  --rsync-path="sudo rsync" \
+  --exclude ".git/" \
+  . debian@51.91.250.220:/var/www/html/euros-per-hora
